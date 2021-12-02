28258076bd4159fd61f3f89e9fd920b13adbedf7
Arunkumar <94372877+arunkumart-tringapps@users.noreply.github.com>
2021-12-02 13:27:22 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+ public class Main {
+static int maxProduct(int arr[], int n) {
+if (n < 3) {
+return -1;
+}
+Arrays.sort(arr);
+return Math.max(arr[0] * arr[1] * arr[n - 1],
+arr[n - 1] * arr[n - 2] * arr[n - 3]);
+}
+public static void main(String[] args) {
+int arr[] = {15, -3, 5, -6, 20};
+int n = arr.length;
+
+int max = maxProduct(arr, n);
+
+if (max == -1) {
+    System.out.println("No Triplet Exists");
+
+} else {
+System.out.println("Maximum product is " + max);
+}
+}
+}
+
